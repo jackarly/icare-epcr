b93091a8fae22dd5bb4c01b7de2acf23874cfb47
jackarly <jcbermudez3033@gmail.com>
2023-06-03 00:27:30 +0800

    seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            UserSeeder::class,
+            MedicSeeder::class,
+            IncidentSeeder::class,
+        ]);
     }
 }
